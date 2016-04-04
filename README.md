0b7ac8132d020d98f2206ed78bd16da079afefa3
Mehmet <soybalim@gmail.com>
2016-04-04 23:01:45 +0300

    > Requirement 1 is ok

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Coursera Android Lab8 - Daily Selfie Project
 
-- [ ] Requirement #1: If the user clicks on the camera icon on the ActionBar, the app will open up a picture-taking app already installed on the device. 
+- [x] Requirement #1: If the user clicks on the camera icon on the ActionBar, the app will open up a picture-taking app already installed on the device.
 - [ ] Requirement #2: If the user now snaps a picture and accepts it, the picture is returned to the DailySelfie app and then displayed in some way to the user along with other selfies the user may have already taken.
 - [ ] Requirement #3: If the user clicks on the small view, then a large view will open up, showing the selfie in a larger format.
--- app/src/main/java/com/example/mehmet/dailyselfie/MainActivity.java
+++ app/src/main/java/com/example/mehmet/dailyselfie/MainActivity.java
@@ -1,52 +1,78 @@
 package com.example.mehmet.dailyselfie;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView mImageView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mImageView = (ImageView) findViewById(R.id.myImageView);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Inflate actions into action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_camera:
+                // User chose the "camera" action
+                dispatchTakePictureIntent();
+                Toast.makeText(getApplicationContext(), "camera!", Toast.LENGTH_SHORT).show();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    // Take a photo with the camera app
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //Get the thumbnail
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
